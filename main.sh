0b47f410312c617139d907ed15fb41a8e08e5ddc
Amin Hajjari <107117597+aminhajjari@users.noreply.github.com>
2026-09-06 15:55:34 +0330

    Update main.sh

--- main.sh
+++ main.sh
@@ -34,7 +34,7 @@ RESULTS_BASE="$PROJECT_DIR/output"
 
 JOB_LOGS_DIR="$PROJECT_DIR/output"
 
-TIMEOUT_DEFAULT=28800 # 4 hours
+TIMEOUT_DEFAULT=28800 # 8 hours
 
 
 #=======================================================================
